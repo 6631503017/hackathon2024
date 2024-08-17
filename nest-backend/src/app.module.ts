6bc0185638af3bf332a9aad1523a7fe83668cf68
ABOUTBLANK <6631503017@lamduan.mfu.ac.th>
2024-08-17 20:57:04 +0700

    backend: add class validator and change port and set DB url

--- nest-backend/src/app.module.ts
+++ nest-backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://admin:admin1234@hackathon.2hpke.mongodb.net/')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
