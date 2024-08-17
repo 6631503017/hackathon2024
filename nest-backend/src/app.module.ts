13120d04a574ff42e4dfbd17bebc655549daba2f
6631503017 <6631503017@lamduan.mfu.ac.th>
2024-08-18 01:34:58 +0700

    backend: import mongo uri

--- nest-backend/src/app.module.ts
+++ nest-backend/src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://admin:admin1234@hackathon.2hpke.mongodb.net/')
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
