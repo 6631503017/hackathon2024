bcc1f1848f31b61ea513e56842c15716a096f7e2
6631503017 <6631503017@lamduan.mfu.ac.th>
2024-08-18 10:06:40 +0700

    backend: fill user dto

--- nest-backend/src/users/dto/create-user.dto.ts
+++ nest-backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,34 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, IsNotEmpty, IsOptional, IsArray, IsEnum, IsMongoId } from 'class-validator';
+import { Types } from 'mongoose';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsArray()
+  @IsOptional()
+  @IsMongoId({ each: true })
+  owned?: Types.ObjectId[];
+
+  @IsArray()
+  @IsOptional()
+  @IsMongoId({ each: true })
+  favorites?: Types.ObjectId[];
+
+  @IsEnum([0, 1, 2])
+  @IsOptional()
+  tier?: number;
+
+  @IsString()
+  @IsOptional()
+  picture?: string;
+}
